6e1eab74ea9e2d6135426de9d7c9ab18418377c9
CreMoProduction <aleks.delta2013@gmail.com>
2021-05-17 20:50:42 +0200

    v0.21 working solution

--- SES.jsx
+++ SES.jsx
@@ -1,35 +1,40 @@
 /*SES.jsx is a 'Set Expression Script' for AE
-v.0.2
+v.0.21
 Указаываю выражение для свойства Opacity от Layer_start до Layer_End слоя с у++ или y-- в активной композии 
 */
 
-var Layer_start=2       //начальный слой
-Layer_End=11            //последний слой
+var Layer_start=6       //начальный слой
+Layer_End=27            //последний слой
 Iteration_Up= true;     //y++ когда true, y-- когда false
 //----------------------------------------------------------------------------
 Switcher_opacity=1;     //переключатель прозрачности, по умполчанию используй 1 
-
+y="";
 //Здесь твое выражение. Пример:'if (thisComp.layer("CONTROL").effect("Selector")("Slider")>='+y+' && thisComp.layer("CONTROL").effect("Selector")("Slider")<= '+y+'.9) 100 else 0;'; 
-var Exp=
-	/*
+function Exp(y) {
+	var exp=
+
 	'if (thisComp.layer("CONTROL TextM").effect("Manual Element")("Checkbox")==0) { \n'+
 	'Q=thisComp.layer("Null Contoller TextM").effect("Case Element")("Slider"); \n'+
 	'if (Q=='+y+') 100 else 0 \n'+
 	'}else {if (thisComp.layer("CONTROL TextM").effect("Element Control")("Slider")>='+y+' && thisComp.layer("CONTROL TextM").effect("Element Control")("Slider")<= '+y+'.9) 100 else 0;};'; 
+	
+	/*
+	'if (thisComp.layer("CONTROL").effect("Selector")("Slider")>='+y+' && thisComp.layer("CONTROL").effect("Selector")("Slider")<= '+y+'.9) 100 else 0;' 
 	*/
-	'if (thisComp.layer("CONTROL").effect("Selector")("Slider")>='+y+' && thisComp.layer("CONTROL").effect("Selector")("Slider")<= '+y+'.9) 100 else 0;'; 
-
+	return exp;
+}
 
 
 
 
 //------------------------------------
+
 y=Switcher_opacity;
 Q=Layer_End-Layer_start;
 if (Iteration_Up==false) {Switcher_opacity=Q+1} else {Q}
 app.beginUndoGroup("SES")
 for (n=Layer_start; n<=Layer_End; n++) {
-	app.project.activeItem.layer(n).property("Opacity").expression=Exp;
+	app.project.activeItem.layer(n).property("Opacity").expression=Exp(y);
 	
 
 if (Iteration_Up==true) {y++} else {y--};
@@ -37,4 +42,4 @@ if (Iteration_Up==true) {y++} else {y--};
 }
 
 app.endUndoGroup();
-
+alert(Q);
